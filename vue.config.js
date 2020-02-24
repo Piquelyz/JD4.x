5a0f7c64f38a4ebaf6c4628a1d2b3e00653be38e
Yongzhi <lyzpique@gmail.com>
2020-02-24 11:48:12 +0100

    add vue.config.js

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    configureWebpack: {
+        devServer: {
+            port: 8089,
+            open: true,
+
+            //mock data
+            before(app) {
+                
+            }
+        }
+    }
+}
