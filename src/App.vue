64fbad6f3e7adbe869334909e6f1c88d25cd92b4
Yongzhi <lyzpique@gmail.com>
2020-02-25 11:20:01 +0100

    register

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/login">登陆</router-link> |
+      <router-link to="/">注册</router-link>
     </div>
     <router-view/>
   </div>
@@ -15,5 +15,13 @@
   -moz-osx-font-smoothing grayscale
   text-align center
   color #2c3e50
-  margin-top 60px
+  margin-top 30px
+
+#nav
+  padding 30px
+  a 
+    font-weight bold
+    color #2c3e50
+    &.router-link-exact-active
+      color #42b983
 </style>
--- vue.config.js
+++ vue.config.js
@@ -6,8 +6,45 @@ module.exports = {
 
             //mock data
             before(app) {
-                
-            }
+            let userpool = [
+              {username: 'yongzhi', password: '666666'},
+              {username: 'yongzhi2', password: '666666'}
+            ]
+            app.get('/api/register', (req, res) => {
+              // es6结构赋值
+              const{ username, password} = req.query
+              const userlength = userpool.filter(u => u.username==username).length
+              if(userlength > 0) {
+                res.json({
+                  success: false,
+                  message: '用户名已存在'
+                })
+              } else {
+                res.json({
+                  success: true,
+                  message: '注册成功'
+                })
+              }
+            })
         }
+      }
+    },
+
+    css: {
+      loaderOptions: {
+        stylus: {
+          'resolve url': true,
+          'import': [
+            './src/theme'
+          ]
+        }
+      }
+    },
+
+    pluginOptions: {
+      'cube-ui': {
+        postCompile: true,
+        theme: true
+      }
     }
 }
